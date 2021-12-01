ad31412f0457a972a41475c3f5a3d2ab573c8c2e
DonoldJTrump <78963458+mememe-ux@users.noreply.github.com>
2021-12-01 15:23:02 +0000

    stuff

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import webbot
 import os
+os.system('/opt/virtualenvs/python3/bin/python3 -m pip install --upgrade pip')
+os.system('pip install webbot')
+os.system('pip install getkey')
+import webbot
 import time
 import random
 from getkey import *
 os.system('pip3 install pyautogui')
-os.system('/opt/virtualenvs/python3/bin/python3 -m pip install --upgrade pip')
 driver = webbot.Browser()
 driver.go_to('google.com')
 
@@ -12,4 +14,4 @@ while True:
 	key = getkey()
 	if key == '!':
 		driver.type("!")
-	driver.go_to('google.com')
+	driver.go_to('medium.com/@FGrante/how-to-install-a-chrome-extension-without-using-the-chrome-web-store-31902c780034')
